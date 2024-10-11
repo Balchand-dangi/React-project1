570a73a8570d79b4c361016864efb2b4350c3aaa
Balchand Dangi <dangibalchand935@gmail.in>
2024-10-12 00:48:42 +0530

    Updates

---
+++ precache-manifest.449b5e170cd28cd57f16200c8fda3db2.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "6d30e33beba58fc43627514a23badb0d",
+    "url": "/React-project1/index.html"
+  },
+  {
+    "revision": "7eedc92d49ee0129a842",
+    "url": "/React-project1/static/css/main.be0c93d8.chunk.css"
+  },
+  {
+    "revision": "5b8e05b2f58a7be6de68",
+    "url": "/React-project1/static/js/2.631a136b.chunk.js"
+  },
+  {
+    "revision": "6c4bfe61775909bc4616",
+    "url": "/React-project1/static/js/3.68d536b4.chunk.js"
+  },
+  {
+    "revision": "7eedc92d49ee0129a842",
+    "url": "/React-project1/static/js/main.5404d1bc.chunk.js"
+  },
+  {
+    "revision": "5bface95b964e8564d31",
+    "url": "/React-project1/static/js/runtime~main.4fed21cb.js"
+  }
+]);
